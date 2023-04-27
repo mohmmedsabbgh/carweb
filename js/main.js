92d63f65c844b6262174d4b2810e68915ac91674
Mohmmed El sabbgh <101419573+mohmmedsabbgh@users.noreply.github.com>
2023-04-27 13:32:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2300,
+    delay: 200
+});
+
+ScrollReveal().reveal('.textSOne', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.photoOne', { delay: 200, origin: 'top' });
+ScrollReveal().reveal('.stowcard', { delay: 200, origin: 'bottom' });
+ScrollReveal().reveal('.cardOne', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.slider', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.sider', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.sider .carousel-item', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.SectionSix .title', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.SectionSix .logoCambny', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.SectionSeven .AllCard', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.footer .Hoquk ', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.taqseem .AllCard ', { delay: 90, origin: 'top' });
+ScrollReveal().reveal('.titlePage ', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.CountentUs .iframe ', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.colorP ', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.form  ', { delay: 100, origin: 'top' });
+ScrollReveal().reveal('.form input ', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.tasees ', { delay: 100, origin: 'bottom' });
+ScrollReveal().reveal('.whatssapp ', { delay: 100, origin: 'bottom' });
